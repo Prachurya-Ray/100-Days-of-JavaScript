37a7b02c29c626dc6e9c57deb7170fd56e37d3ac
Prachurya Ray <prachuryaray@gmail.com>
2024-04-18 01:02:12 +0530

    day 3 added

---
+++ Day-03.js
@@ -0,0 +1,32 @@
+/*
+
+Interview Question: Count Occurrences of Character
+
+Task:
+Write a function called countChar that takes two parameters: a string and a character to count. The function should return the number of times the specified character appears in the string.
+
+Todo Constraints:
+
+> The function should be case-sensitive.
+> The function should handle both lowercase and uppercase characters.
+> The character parameter can be any printable ASCII character (the function should accept any character that is part of the ASCII character set and is printable).
+
+*/
+
+//--------------------------------------------------------------------------------------------------------
+
+h1=document.querySelector('h1');
+h1.innerText="Day 3";
+
+function countChar(str, char){
+    str=str.toLowerCase();
+    char=char.toLowerCase();
+    let count=0;
+    for(i in  str)  {
+        if (str[i] == char) 
+            count++;
+    }
+    return count;
+}
+
+console.log(countChar("Missis@s@ippi# hello@ @@ @@@", "@")); 
